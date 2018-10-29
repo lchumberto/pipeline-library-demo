b062e497246b2c83eb56686537819aef29ec7331
Humberto <lchumberto90@gmail.com>
2018-10-29 01:51:15 -0600

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,14 @@
-#!/usr/bin/env groovy
-
-def call(String name = 'human') {
-  echo "Hello, ${name}."
+#!/usr/bin/groovy
+def build() {
+  "echo mvn clean deploy"
 }
-
+def test(name) {
+  echo "/usr/local/bin/${name}"
+}
+def deploy(env,app) {
+  echo "aws cloudformation create-stack "
+  echo "--stack-name ${env}_{app)"
+  echo "ParameterKey=Env,ParameterValue=${env}"
+}
+// AimTheory have a recommendation and explanation about this here
+return this
